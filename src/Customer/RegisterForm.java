fdb4f1ba2701015e0dd88906940627a70a31a068
Nu'man Arsengbaramae <numan7031@hotmail.com>
2017-10-07 03:04:21 +0700

    เพิ่มข้อมูลusername and password
    แต่ยังไม่ได้กำหนดว่าusernameห้ามซ้ำกัน

--- src/Customer/RegisterForm.java
+++ src/Customer/RegisterForm.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.GroupLayout;
@@ -18,6 +19,10 @@ import javax.swing.JRadioButton;
 import java.awt.Label;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -27,11 +32,18 @@ import com.toedter.calendar.JDateChooser;
 
 import javax.swing.JPasswordField;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class RegisterForm {
 
 	private JFrame frame;
 	private JTextField usernametf;
 	private JPasswordField passwordtf;
+	String query, sql, driver;
+	Statement stmt;
+	ResultSet rs;
+	Connection conn;
 
 	/**
 	 * Launch the application.
@@ -60,6 +72,25 @@ public class RegisterForm {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+try {
+			
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+
+			conn = DriverManager
+					.getConnection("jdbc:ucanaccess://C:/Users/numan/git/Project/AdvoopPrj/prjoop.accdb");
+			//C:\Users\numan\git\Project\AdvoopPrj\prjoop.accdb
+
+			query = "SELECT * FROM com";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+		} catch (Exception eb) {
+			System.err.println(eb);
+
+		}
+		
+		
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 533, 337);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -80,6 +111,7 @@ public class RegisterForm {
 		
 		JButton btnLogin = new JButton("Sign up");
 		
+		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Company");
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Appllicant");
@@ -95,6 +127,71 @@ public class RegisterForm {
 		lblCandidateStatus.setFont(new Font("Tahoma", Font.BOLD, 18));
 		
 		passwordtf = new JPasswordField();
+		
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String dio = null;
+				if(rdbtnNewRadioButton.isSelected()){
+					dio = rdbtnNewRadioButton.getText();
+				}else if(rdbtnNewRadioButton_1.isSelected()){
+					dio = rdbtnNewRadioButton_1.getText();
+				}
+				
+				String un = usernametf.getText();
+				String ps = new String(passwordtf.getPassword());
+				
+				sql = "insert into cus (username,password,status) values('"
+						+ un + "','"
+						+ ps + "','"
+						+ dio + "')";
+				
+
+				if(dio.equals("Company")){
+					
+					sql = "insert into com (username,password,comstatus) values('"
+							+ un + "','"
+							+ ps + "','"
+							+ dio + "')";
+					
+				}else if(dio.equals("Appllicant")){
+					
+					sql = "insert into cus (username,password,cusstatus) values('"
+							+ un + "','"
+							+ ps + "','"
+							+ dio + "')";
+					
+				}
+				
+				
+				try {
+
+					stmt.executeUpdate(sql);
+					
+					if (stmt.executeUpdate(sql) != 0) {
+						JOptionPane.showMessageDialog(null,
+								"บันทึกข้อมูลเรียบร้อยแล้ว");}else{
+									JOptionPane.showMessageDialog(null,
+											"ERROR");}
+
+
+						/*rs.first();
+						textField1.setText(rs.getString(2));
+						textField2.setText(rs.getString(3));
+						textField3.setText(rs.getString(4));*/
+					
+
+				} catch (Exception err) {
+					err.printStackTrace();
+				}
+				
+				usernametf.setText(null);
+				passwordtf.setText(null);
+
+				
+			}
+		});
+		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -106,26 +203,27 @@ public class RegisterForm {
 					.addContainerGap())
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(21)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblCandidateStatus)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addComponent(lblPassword)
-							.addComponent(lblUsername)))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(rdbtnNewRadioButton)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnNewRadioButton_1))
-						.addComponent(usernametf, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE)
-						.addComponent(passwordtf, Alignment.TRAILING))
+							.addGap(289)
+							.addComponent(btnCancel)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnLogin))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblCandidateStatus)
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+									.addComponent(lblPassword)
+									.addComponent(lblUsername)))
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(rdbtnNewRadioButton)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(rdbtnNewRadioButton_1))
+								.addComponent(usernametf, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE)
+								.addComponent(passwordtf, Alignment.TRAILING))))
 					.addContainerGap(57, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(301)
-					.addComponent(btnCancel)
-					.addGap(18)
-					.addComponent(btnLogin)
-					.addContainerGap(76, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -154,11 +252,11 @@ public class RegisterForm {
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(passwordtf, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblPassword))
-					.addGap(12)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnCancel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnCancel, GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
 						.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
-					.addGap(119))
+					.addGap(35))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}
---
+++ src/Customer/command.java
@@ -0,0 +1,59 @@
+package Customer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class command {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out
+				.println("enter data base want see 1=com 2=cus 3=applying 4=offer");
+		String data = input.nextLine();
+
+		try {
+
+			Connection conn = DriverManager
+					.getConnection("jdbc:ucanaccess://C:/Users/numan/git/Project/AdvoopPrj/prjoop.accdb");
+			Statement s = conn.createStatement();
+
+			// s.executeUpdate("insert into lab6 (sakul,country,cast) values('fsf','Gafme',12345)");
+			// s.executeUpdate("insert into lab6 (sakul,country,cast) values('rwfe','32212',3223)");
+
+			
+			ResultSet rs = s.executeQuery("SELECT * FROM cus");
+
+			while (rs.next()) {
+				// System.out.println(rs.getString(1));
+				if (data.equals("1")) {
+					System.out.println("\n" + rs.getString(1) + "\t"
+							+ rs.getString(2) + "\t" + rs.getString(3) + "\t"
+							+ rs.getString(4) + "\t" + rs.getString(5) + "\t"
+							+ rs.getString(6) + "\t" + rs.getString(7) + "\t"
+							+ rs.getString(8) + "\t" + rs.getString(9) + "\t"
+							+ rs.getString(10));
+				} else if (data.equals("2")) {
+					System.out.println("\n" + rs.getString(1) + "\t"
+							+ rs.getString(2) + "\t" + rs.getString(3) + "\t"
+							+ rs.getString(4) + "\t" + rs.getString(5) + "\t"
+							+ rs.getString(6) + "\t" + rs.getString(7) + "\t"
+							+ rs.getString(8) + "\t" + rs.getString(9) + "\t"
+							+ rs.getString(10) + "\t" + rs.getString(11) + "\t"
+							+ rs.getString(12) + "\t" + rs.getString(13) + "\t"
+							+ rs.getString(14));
+
+				}
+			}
+		} catch (Exception e) {
+
+			System.out.println(e);
+
+		}
+
+	}
+}
