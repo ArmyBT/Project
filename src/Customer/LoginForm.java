5679ca2147065f8a569c2a352849119092c3dbf9
Nu'man Arsengbaramae <numan7031@hotmail.com>
2017-10-07 23:14:50 +0700

    หน้าล็อกอินเก่าใช้ไม่ได้

---
+++ src/Customer/LoginForm.java
@@ -0,0 +1,166 @@
+package Customer;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+import Employee.Search;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LoginForm {
+
+	JFrame frame;
+	private JTextField usernametf;
+	private JPasswordField passwordtf;
+	String query, sql, driver;
+	Statement stmt;
+	ResultSet rs;
+	Connection conn;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginForm window = new LoginForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+        try {
+			
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+
+			conn = DriverManager.getConnection("jdbc:ucanaccess://C:/AdvoopPrj/prjoop.accdb");
+
+			//C:\Users\numan\git\Project\AdvoopPrj\prjoop.accdb
+
+			query = "SELECT * FROM com";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			
+		} catch (Exception eb) {
+			System.err.println(eb);
+
+		}
+		
+		
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
+		lblLogin.setBounds(25, 11, 91, 66);
+		frame.getContentPane().add(lblLogin);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblUsername.setBounds(58, 88, 118, 32);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblPassword.setBounds(58, 131, 118, 32);
+		frame.getContentPane().add(lblPassword);
+		
+		usernametf = new JTextField();
+		usernametf.setBounds(186, 98, 214, 20);
+		frame.getContentPane().add(usernametf);
+		usernametf.setColumns(10);
+		
+		passwordtf = new JPasswordField();
+		passwordtf.setBounds(186, 141, 214, 20);
+		frame.getContentPane().add(passwordtf);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String ps = new String(passwordtf.getPassword());
+				try{
+					String sql = " SELECT * FROM  login " +" WHERE username = ? " +" AND password = ? " ;
+					 
+					PreparedStatement pre;
+						pre = conn.prepareStatement(sql);
+						pre.setString(1, usernametf.getText());
+						pre.setString(2,ps);
+						
+						ResultSet rs = pre.executeQuery();
+						
+						if(rs.next()) {
+							//JOptionPane.showMessageDialog(null, "Correct Username/Password");
+							String sLevel = rs.getString(4);
+							if(sLevel.equals("Company")) {
+								Search window = new Search();
+								window.frame.setVisible(true);
+								frame.dispose();
+								// Menu Admin
+								
+								} else if (sLevel.equals("Appllicant")){
+									
+									Home window4 = new Home();
+									window4.frame.setVisible(true);
+									frame.dispose();
+								}
+							
+						} else {
+						JOptionPane.showMessageDialog(null, "Incorrect Username/Password");
+						}
+						} catch (Exception ex) {
+						JOptionPane.showMessageDialog(null, ex.getMessage());
+						System.out.println(ex);
+						}
+				
+			}
+		});
+		btnLogin.setBounds(311, 185, 89, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		JButton btnCancle = new JButton("Cancle");
+		btnCancle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				usernametf.setText(null);
+				passwordtf.setText(null);
+				
+			}
+		});
+		btnCancle.setBounds(212, 185, 89, 23);
+		frame.getContentPane().add(btnCancle);
+	}
+}
